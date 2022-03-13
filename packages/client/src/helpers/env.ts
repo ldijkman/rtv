7a2cacd358c05ebafd9becce792dc20bc2ef1579
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-03-13 11:32:31 +0100

    Delete env.ts

--- packages/client/src/helpers/env.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Environment helper
- */
-declare const IS_BROWSER: true | undefined;
-
-/**
- * IS_BROWSER is set by rollup in browser build. In Node.js we have ReferenceError here
- */
-export const IS_NODE = typeof IS_BROWSER === 'undefined';
